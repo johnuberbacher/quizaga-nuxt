7ea2f8ac6a9732ab13009dd9e1a0de1d15e4b1a2
John Uberbacher <johnuberbacher@gmail.com>
2023-03-30 10:49:51 -0600

    Updates

--- app.vue
+++ app.vue
@@ -2,6 +2,7 @@
   <Head>
     <Title>Quizaga</Title>
   </Head>
+  <GitHubCorner></GitHubCorner>
     <div
       class="bg-purple-800 select-none p-4 min-h-screen overflow-hidden flex flex-col items-center justify-start"
     >
