7ddd9fb7ce065bbe2c0c5c14651ddb7ad3e40c20
John Uberbacher <johnuberbacher@gmail.com>
2023-03-30 11:07:31 -0600

    Updates

--- app.vue
+++ app.vue
@@ -4,7 +4,8 @@
   </Head>
   <GitHubCorner></GitHubCorner>
     <div
-      class="bg-purple-800 select-none p-4 min-h-screen overflow-hidden flex flex-col items-center justify-start"
+      class="bg-purple-800 select-none p-4 min-h-screen overflow-hidden flex flex-col items-center justify-start bg-cover bg-no-repeat bg-center"
+      style="background-image: url('bg.jpg');"
     >
       <NuxtPage />
     </div>
